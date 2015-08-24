db870f4e667cbd25671da90db868c1e9ab64df3f
Piotrek <p.swiatowski@gmail.com>
2015-08-24 18:04:03 +0100

    add rediret controller

---
+++ src/main/java/com/swiatowski/bitly/rest/controllers/RedirectController.java
@@ -0,0 +1,35 @@
+package com.swiatowski.bitly.rest.controllers;
+
+import com.swiatowski.bitly.core.models.entities.Link;
+import com.swiatowski.bitly.core.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Piotrek on 8/24/2015.
+ */
+@Controller
+public class RedirectController {
+
+    private LinkService linkService;
+
+    @Autowired
+    public RedirectController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @RequestMapping(value = "/{encodeUrl}", method = RequestMethod.GET)
+    @ResponseBody
+    public void sendRedirect(@PathVariable String encodeUrl, HttpServletResponse httpServletResponse) throws IOException {
+        Link link = linkService.findByEncodeName(encodeUrl);
+        String url = link.getUrl();
+        httpServletResponse.sendRedirect(url);
+    }
+}
