1ec91544de66e07cea87b9e3a3a9475c523be776
Piotrek <p.swiatowski@gmail.com>
2015-08-24 15:37:11 +0100

    set up web-app microservice with spring-boot/java/h2/gradle

---
+++ src/main/java/com/swiatowski/bitly/Application.java
@@ -0,0 +1,18 @@
+package com.swiatowski.bitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by Piotrek on 8/24/2015.
+ */
+@EnableWebMvc
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
